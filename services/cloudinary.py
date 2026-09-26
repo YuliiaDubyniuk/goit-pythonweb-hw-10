19c2a844663ae8089bb34ed5acdf0ee231465685
YuliiaDubyniuk <yuliia.dubyniuk@gmail.com>
2026-09-26 00:43:46 -0400

    fix: add missing dependency, update cloudinary name

--- services/cloudinary.py
+++ services/cloudinary.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 cloudinary.config(
-    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    cloud_name=os.environ.get("CLOUDINARY_NAME"),
     api_key=os.environ.get("CLOUDINARY_API_KEY"),
     api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
     secure=True,
